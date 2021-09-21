0a7a50abd3e4fc258d91fdd090af96d4baddef9c
p22rstewart <p22rstewart@fairfieldprep.org>
2021-09-21 10:11:15 -0400

    ye

---
+++ lab_6-1.py
@@ -0,0 +1,7 @@
+# Author: Ryan (AMDG) 9/21/21
+first_name = input("what is your first name? ")
+last_name = input("what is your last name? ")
+
+full_name = first_name + " " + last_name
+
+print(full_name)
